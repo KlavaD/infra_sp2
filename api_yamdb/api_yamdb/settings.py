a7401e1ed9dcbd7bcdcd98e7d24c0c20eef4a669
Клавдия Дунаева <klavam@yandex.ru>
2023-03-10 23:30:29 +0700

    докер-спринт1-3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -103,7 +103,7 @@ STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../reviews/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
